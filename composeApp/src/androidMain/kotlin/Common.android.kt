67d8b074ec6195cc016110778e75d7850b41ae54
Cristhian Bonilla <cristhian4545@gmail.com>
2024-07-09 17:27:04 -0400

    independent desing per platform

--- composeApp/src/androidMain/kotlin/Common.android.kt
+++ composeApp/src/androidMain/kotlin/Common.android.kt
@@ -1,5 +1,37 @@
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.common.ImageCarousel
+import com.example.common.MainTextContent
+import com.example.common.SearchTextField
 
 @Composable
 actual fun PlatformSpecificMainContent() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        ImageCarousel(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(300.dp)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        MainTextContent(
+            modifier = Modifier.fillMaxWidth(),
+            fontSize = 24
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        SearchTextField()
+    }
 }
--- composeApp/src/iosMain/kotlin/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/MainViewController.kt
@@ -1,3 +1,4 @@
 import androidx.compose.ui.window.ComposeUIViewController
+import com.example.common.App
 
 fun MainViewController() = ComposeUIViewController { App() }
