4666f478158a3016ae0539b7695354c1372fdddc
Cristhian Bonilla <cristhian.bonilla@sephora.com>
2024-07-21 00:31:11 -0400

    preparar login para todos

--- composeApp/src/commonMain/kotlin/auth/data/api/LoginApiImpl.kt
+++ composeApp/src/commonMain/kotlin/auth/data/api/LoginApiImpl.kt
@@ -10,7 +10,7 @@ import io.ktor.http.contentType
 
 class LoginApiImpl(private val httpClient: HttpClient) : LoginApi {
     override suspend fun login(loginEntity: LoginRequestEntity): HttpResponse {
-        return httpClient.post("http://0.0.0.0:8081/login") {
+        return httpClient.post("https://b46a-198-154-183-1.ngrok-free.app/login") {
             contentType(ContentType.Application.Json)
             setBody(loginEntity)
         }
